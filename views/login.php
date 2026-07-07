9fbfb8d36f52f2d7dfdb311871333c5a2060b413
Soknan Coding <poch.soknan@gmail.com>
2026-07-07 11:56:57 +0700

    Updata link sana on login page just one line.!

--- views/login.php
+++ views/login.php
@@ -520,7 +520,7 @@ unset($_SESSION['login_email']);
         <div class="brand-lines"></div>
 
         <div class="brand-top">
-            <a href="#" class="brand-logo">
+            <a href="home.php" class="brand-logo">
                 <div class="brand-logo-mark">
                     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 100 4 2 2 0 000-4z"/>
